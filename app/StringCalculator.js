ca30eb7d68bc0b476cf866ca42df892aa70232c2
rpass <robertmpassmore@gmail.com>
2016-02-18 13:54:42 +0200

    Attempt: Real code kata

--- app/StringCalculator.js
+++ app/StringCalculator.js
@@ -1,4 +1,58 @@
 /**
  * Created by robert.passmore on 2016/02/15.
  */
-console.log('Hi');
+
+var StringCalculator = function () {
+};
+
+StringCalculator.prototype.hasCustomDelimiterDescribed = function (input) {
+    var delimiterDescriptionRE = /\/\/(.*)\n/;
+    return delimiterDescriptionRE.test(input);
+};
+
+StringCalculator.prototype.ExtractCustomDelimiter = function (input) {
+    var delimiterDescriptionRE = /\/\/(.)\n/;
+    var longDelimiterDescriptionRE = /\/\/\[(.*)\]\n/;
+    var delimiter = input.match(delimiterDescriptionRE)[1];
+    console.log(delimiter);
+    return delimiter;
+}
+
+StringCalculator.prototype.Add = function (input) {
+    var sum = 0;
+    var delimiters = [',', '\n'];
+    var delimitersRE = new RegExp(delimiters.join('|'));
+    if (input !== '') {
+
+        if (this.hasCustomDelimiterDescribed(input)) {
+            var custDelimiters = this.ExtractCustomDelimiter(input);
+            delimiters.push(custDelimiters);
+            delimitersRE = new RegExp(delimiters.join('|'));
+            input = input.substr(input.indexOf('\n'));
+        }
+
+        var operands = input.split(delimitersRE).map(function (x) {
+            if(+x < 1000){
+                return +x;
+            } else {
+                return 0;
+            }
+        });
+
+        var illegalOperands = [];
+        var errorMessage = "negatives not allowed: %";
+        operands.map(function (x) {
+            if (x < 0) illegalOperands.push(x);
+        });
+        if (illegalOperands.length > 0) {
+            throw new Error(errorMessage.replace('%', illegalOperands.join(', ')));
+        }
+
+        for (var i = 0; i < operands.length; i++) {
+            sum += operands[i];
+        }
+    }
+
+    return sum;
+}
+;
--- test/StringCalculator.test.js
+++ test/StringCalculator.test.js
@@ -1,6 +1,115 @@
 /**
  * Created by robert.passmore on 2016/02/15.
  */
-describe('', function () {
 
-});
+// tests are here!
+describe('StringCalculator', function () {
+    var strCalc;
+    beforeEach(function () {
+        strCalc = new StringCalculator();
+    });
+
+    it('should take an empty string as input and returns 0', function () {
+        var input = '';
+        var actual = strCalc.Add(input);
+        var expected = 0;
+
+        expect(actual).toEqual(expected);
+    });
+    it('should take a single input as input and returns it', function () {
+        var input = '4';
+        var actual = strCalc.Add(input);
+        var expected = 4;
+
+        expect(actual).toEqual(expected);
+    });
+
+    it('should take 2 inputs and returns their sum', function () {
+        var input = '1,4';
+        var actual = strCalc.Add(input);
+        var expected = 5;
+
+        expect(actual).toEqual(expected);
+    });
+
+    it('should take any number of inputs and return their sum', function () {
+        var input = '2,4,2';
+        var actual = strCalc.Add(input);
+        var expected = 8;
+
+        expect(actual).toEqual(expected);
+    });
+
+    it('should take any number of inputs delimited by new lines and return their sum', function () {
+        var input = '3\n4\n3';
+        var actual = strCalc.Add(input);
+        var expected = 10;
+
+        expect(actual).toEqual(expected);
+    });
+
+    it('should take any number of inputs delimited by new lines and commas and return their sum', function () {
+        var input = '3\n4,3';
+        var actual = strCalc.Add(input);
+        var expected = 10;
+
+        expect(actual).toEqual(expected);
+    });
+
+    it('should take any number of inputs delimited by custom Delimiters and return their sum', function () {
+        var input = '//~\n3~4~3';
+        var actual = strCalc.Add(input);
+        var expected = 10;
+
+        expect(actual).toEqual(expected);
+    });
+
+    describe('hasCustomDelimiterDescription', function () {
+        it('should return true if a given input has a custom delimiter described', function () {
+            var input = '//~\n3~4~3';
+            var actual = strCalc.hasCustomDelimiterDescribed(input);
+            var expected = true;
+
+            expect(actual).toEqual(expected);
+        });
+    });
+
+    describe('ExtractCustomDelimiter', function () {
+        it('should take an input with a customer delimiter described and return the delimiter', function () {
+            var input = '//~\n3~4~3';
+            var actual = strCalc.ExtractCustomDelimiter(input);
+            var expected = '~';
+
+            expect(actual).toEqual(expected);
+        });
+
+        it('should take an input with a customer delimiter of any length described and return the delimiter', function () {
+            var input = '//[~~~]\n3~~~4~~~3';
+            var actual = strCalc.ExtractCustomDelimiter(input);
+            var expected = '~~~';
+
+            expect(actual).toEqual(expected);
+        });
+
+    })
+
+    it('should accept input with negative nuymbers but throw an error and list the negative nuymbers', function () {
+        var input = '1,4,-2,-4';
+        var actual = function () {
+            strCalc.Add(input);
+        };
+        var expected = 'negatives not allowed: -2, -4';
+
+        expect(actual).toThrowError(expected);
+    });
+
+    it('should ignore input with nuymbers bigger than 1000', function () {
+        var input = '3,4,5,1000';
+        var actual = strCalc.Add(input);
+        var expected = 12;
+
+        expect(actual).toEqual(expected);
+    });
+
+
+})
