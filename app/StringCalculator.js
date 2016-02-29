c26e7bbf671131b3171e36ff8314d8820c57fcb2
rpass <robertmpassmore@gmail.com>
2016-02-29 17:07:08 +0200

    Attempt: less verbose kata

--- app/StringCalculator.js
+++ app/StringCalculator.js
@@ -1,4 +1,28 @@
 /**
  * Created by robert.passmore on 2016/02/15.
  */
-console.log('Hi');
+var Add = function (input) {
+    var sum = 0;
+    var patternsToReplace = /[\n]/g;
+    var customDelimiterPattern = /\/\/(.)|(\[(.+)\])\n/;
+    if (input !== '') {
+        if (customDelimiterPattern.test(input)) {
+            patternsToReplace = new RegExp(input.match(customDelimiterPattern)[1],'g');
+            input = input.split('\n')[1];
+        }
+        input = input.replace(patternsToReplace, ',');
+        console.log(input);
+        var inputs = input.split(',').map(function (x) {
+            return +x;
+        });
+        inputs = inputs.filter(function (x) {
+            console.log(x);
+            return x < 1000;
+        })
+
+        sum = inputs.reduce(function (prev, curr) {
+            return prev + curr;
+        });
+    }
+    return sum;
+};
--- test/StringCalculator.test.js
+++ test/StringCalculator.test.js
@@ -1,6 +1,35 @@
 /**
  * Created by robert.passmore on 2016/02/15.
  */
-describe('', function () {
+describe('StringCalculator', function () {
+    it('adds', function () {
+        expect(Add('')).toEqual(0);
+    });
+    it('takes single input and returns it', function () {
+        expect(Add('3')).toEqual(3);
+    });
 
+    it('takes two inputs and returns sum', function () {
+        expect(Add('3,2')).toEqual(5);
+    });
+
+    it('takes any number of inputs and return their sum', function () {
+        expect(Add('3,2,5')).toEqual(10);
+    });
+
+    it('takes any number of inputs, seperated by new lines and return their sum', function () {
+        expect(Add('3\n2\n5')).toEqual(10);
+    });
+
+    it('takes any number of inputs, seperated by a custom delimiter and return their sum', function () {
+        expect(Add('//~\n3~2~5')).toEqual(10);
+    });
+
+    it('takes any number of inputs, ignoring those > 1000 and return their sum', function () {
+        expect(Add('3,2,5,1003')).toEqual(10);
+    });
+
+    it('takes any number of inputs, seperated by a custom delimiter of length > 1 and return their sum', function () {
+        expect(Add('//[~~]\n3~~2~~5')).toEqual(10);
+    });
 });
